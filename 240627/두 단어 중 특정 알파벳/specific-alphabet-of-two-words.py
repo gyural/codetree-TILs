51d22adc2dbf40ec119de5385833a98a10c2871c
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-06-27 16:19:42 +0900

    Title: 두 단어 중 특정 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/두 단어 중 특정 알파벳/specific-alphabet-of-two-words.py
@@ -0,0 +1,44 @@
+from itertools import combinations
+
+def get_set_word(idx, arrays, temp, result):
+    if(len(arrays) == idx):
+        result.append(temp)
+        temp = []
+        return
+    else:
+        for w in arrays[idx]:            
+            get_set_word(idx+1, arrays, temp+[w], result)
+    return result
+
+length = ord('z')-ord('a')+1
+result = [0 for i in range(length)]
+
+
+
+
+N = int(input())
+words = []
+
+# word 입력받기
+for i in range(N):
+    a, b = input().split()
+    words.append([a,b])
+set_word = get_set_word(0, words, [], [])
+
+for combi in set_word:
+    a, b, c = combi
+    d = a+b+c
+
+    obj = {}
+    for char in d:
+        if(char in obj.keys()):
+            obj[char] += 1
+        else:
+            obj[char] = 1
+
+    for key, val in obj.items():
+        idx = ord(key)-ord('a')
+        result[idx] = max(result[idx], val)
+
+for val in result:
+    print(val)
